de54b8c04927f80c619f1f615aadde9b8bf56b6f
Pieterjan <pieterjandeclippel@msn.com>
2023-08-06 20:51:26 +0200

    Merge iconextractor (#4)
    * Temp commit
    
    * Moved project to this solution
    
    * Update workflows
    
    * Update readme
    
    * Update readme
    
    * Nullability

---
+++ MintPlayer.IconUtils/Exceptions/ExtractException.cs
@@ -0,0 +1,12 @@
+﻿namespace MintPlayer.IconUtils.Exceptions;
+
+public class ExtractException : Exception
+{
+    public ExtractException(string message) : base(message)
+    {
+    }
+
+    public ExtractException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
---
+++ MintPlayer.PlatformBrowser/Enums/EFileType.cs
@@ -0,0 +1,29 @@
+﻿namespace MintPlayer.PlatformBrowser.Enums;
+
+public enum EFileType
+{
+    crx,
+    download,
+    f4v,
+    flv,
+    htm,
+    html,
+    mht,
+    mhtm,
+    mhtml,
+    mx5skin,
+    nex,
+    opdownload,
+    pdf,
+    rss,
+    safariextz,
+    shtm,
+    shtml,
+    svg,
+    svgz,
+    webarchive,
+    webp,
+    xht,
+    xhtml,
+    xml
+}
